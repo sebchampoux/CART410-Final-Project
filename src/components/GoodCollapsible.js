b53d8b3e2679401b35c0597b104ae6d2a9e48d19
Sébastien Champoux <contact@sebastienchampoux.com>
2021-11-02 11:34:40 -0400

    Add hidden on collapsibles

--- src/components/GoodCollapsible.js
+++ src/components/GoodCollapsible.js
@@ -28,6 +28,7 @@ const GoodCollapsible = () => {
 				id="collapsibleContent1"
 				role="region"
 				aria-labelledby="collapsibleHead1"
+				hidden={!isOpen}
 			>
 				<p>
 					Put a bird on it cliche twee pour-over, fixie chambray migas iceland biodiesel vice messenger bag tumeric marfa salvia. Tumeric banh mi fixie man bun, salvia cornhole irony kombucha plaid hashtag humblebrag. Kickstarter humblebrag jianbing cray fam edison bulb bitters snackwave disrupt irony. Pour-over drinking vinegar fam hammock. Taiyaki pug bitters, hella fam everyday carry locavore banjo mustache tbh adaptogen meggings austin. Polaroid fixie yuccie mlkshk cronut kogi.
